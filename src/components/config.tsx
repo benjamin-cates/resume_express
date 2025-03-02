2580d630a72996f3f62e2c6ceed8142759bf801f
Benjamin Cates <thebenjicat@gmail.com>
2025-03-02 11:34:52 -0800

    Fancy checkbox

--- src/components/config.tsx
+++ src/components/config.tsx
@@ -8,8 +8,7 @@ interface Props {
 }
 const ConfigComponent: React.FC<Props> = (props: Props) => {
   const checkbox = (formal_name: string, name: string) => (
-    <div>
-      &nbsp;&nbsp;
+    <label htmlFor={formal_name} className="checkbox_wrapper">
       <input
         type="checkbox"
         id={formal_name}
@@ -17,16 +16,17 @@ const ConfigComponent: React.FC<Props> = (props: Props) => {
           props.update_func!(["config", formal_name], e.target.checked)
         }
         checked={(props.config as any)[formal_name]}
-      ></input>
-      <label htmlFor={formal_name}>{name}</label>
-    </div>
+        className={(props.config as any)[formal_name] ? "active" : "inactive"}
+      ></input>&nbsp;&nbsp;
+      {name}
+    </label>
   );
   return (
-    <>
+    <div className="checkbox_holder">
       {checkbox("is_two_column", "Use Two Columns")}
       {checkbox("is_a4", "A4 Sizing")}
       {checkbox("is_locked", "Lock Editing")}
-    </>
+    </div>
   );
 };
 export default ConfigComponent;
